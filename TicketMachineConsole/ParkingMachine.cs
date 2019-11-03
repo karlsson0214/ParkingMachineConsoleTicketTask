6674a0730269f81d4938f73e8d8c5036049edb1f
Karlsson <karlsson0214@gmail.com>
2019-11-04 00:08:28 +0100

    Add project files.

---
+++ TicketMachineConsole/ParkingMachine.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParkingMachineConsoleTicket
+{
+    /// <summary>
+    /// An object of the class ParkingMachine represents a parking machine.
+    /// 
+    /// Insert money first. 
+    /// 
+    /// Then buy a ticket or select cancel to refund.
+    /// </summary>
+    public class ParkingMachine
+    {
+        // There is two places to save money.
+        // Total in the machine, from all finnished purchases.
+        private int total;
+
+        
+        private int currentTotal;
+
+        // Cost to park.
+        private int costPerHour;
+
+        public ParkingMachine(int costPerHour)
+        {
+            total = 0;
+            currentTotal = 0;
+            this.costPerHour = costPerHour;
+        }
+
+    }
+}
